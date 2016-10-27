b8957b2815352665329358768a92aca456bc3766
Productzziejo <kenzziejo@gmail.com>
2016-10-27 12:59:11 -0600

    week 7

--- indv-website/home-page.php
+++ indv-website/home-page.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML> 
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>index.html</title>
+ 
+    <?php include("modules/header.php");?>
+    
 </head>
 
 <body>
@@ -64,34 +65,14 @@
 
 <section id="section-four"> 
           <h1>Join Us</h1>
-        <p>We challenge you to dive. To Jump. To fall. To FruitFall.  Don't procrastinate this any more. </p>
+        <p>We challenge you to dive. To Jump. To fall. To love fruit.  Don't procrastinate this any more. </p>
          
 </section>   
       
   
 <footer> 
     
-    <h4>Follow us on</h4>
-    <a href="https://www.facebook.com/" target="_blank">
-        <img src="images/facebook.png" alt="facebook logo">
-    </a>
-     <a href="https://twitter.com/" target="_blank">
-        <img src="images/twitter.png" alt="twitter logo">
-    </a>
-     <a href="https://www.instagram.com/" target="_blank">
-        <img src="images/instagram.png" alt="instagram logo">
-    </a>
-    
-    <br>
-    
-            <p><a href="indv-website/terms.html">Terms</a></p>
-            <p><a href="about-us.html">About Us</a></p>
-            <p><a href="contactinfo.html">Contact Information</a></p>
-            <p><a href="week4-ponder-prove-anchors.html#ho">Hours of Operation</a></p>
-    
-           <p>&copy; 2016 Fruit Falling<p>
-    
-          <p>&copy; 2016 Fruit Falling || Makenzie Clayson <p> 
+  <?php include("modules/footer.php"); ?>
           
 
         </footer>
---
+++ indv-website/modules/footer.php
@@ -0,0 +1,21 @@
+   <h4>Follow us on</h4>
+    <a href="https://www.facebook.com/" target="_blank">
+        <img src="images/facebook.png" alt="facebook logo">
+    </a>
+     <a href="https://twitter.com/" target="_blank">
+        <img src="images/twitter.png" alt="twitter logo">
+    </a>
+     <a href="https://www.instagram.com/" target="_blank">
+        <img src="images/instagram.png" alt="instagram logo">
+    </a>
+    
+    <br>
+    
+            <p><a href="indv-website/terms.html">Terms</a></p>
+            <p><a href="about-us.html">About Us</a></p>
+            <p><a href="contactinfo.html">Contact Information</a></p>
+            <p><a href="week4-ponder-prove-anchors.html#ho">Hours of Operation</a></p>
+    
+           <p>&copy; 2016 Fruit Falling<p>
+    
+          <p>&copy; 2016 Fruit Falling || Makenzie Clayson <p> 
